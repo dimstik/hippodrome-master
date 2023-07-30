104edb9cabd6c1d9137200fc060a2174cda12ed5
Dima Pavlov <dima.p96@mail.ru>
2023-07-30 14:58:28 +0300

    create isNull

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -3,6 +3,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HippodromeTest {
+    @Test
+    public void isNull() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Hippodrome(null));
+    }
 
     @Test
     void getHorses() {
