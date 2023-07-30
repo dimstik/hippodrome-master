0f6f4505a3e6d2aadc885b1f126ec39294c45f51
Dima Pavlov <dima.p96@mail.ru>
2023-07-30 15:10:26 +0300

    create isNull

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -3,6 +3,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HorseTest {
+    @Test
+    public void isNull() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Horse(null, 1, 1)
+        );
+    }
 
     @Test
     void getName() {
